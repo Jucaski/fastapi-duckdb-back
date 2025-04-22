b8ee2299b7a9a8e4644884d12348f3718f435a8e
Jucaski <thanididbefore@gmail.com>
2025-04-22 15:35:23 -0600

    adding unique columns endpoint

--- import_csv.py
+++ import_csv.py
@@ -1,14 +1,27 @@
 import duckdb
 import numpy
+from fastapi.encoders import jsonable_encoder
 
 con = duckdb.connect("my_database.db")
+
 def create():
-    con.sql("CREATE TABLE deaths AS FROM read_csv_auto('db/test.csv');")
+    con.sql("CREATE TABLE  AS FROM read_csv_auto('db/test.csv');")
     con.sql("COPY deaths FROM 'db/test.csv'")
     con.close()
 
 def show():
-    tables = con.sql(f"SELECT * FROM deaths")
+    tables = con.sql("SELECT * FROM deaths")
     result = tables.to_df().to_dict(orient="records")
     con.close()
     return result
+
+def columns():
+    rel = con.sql(f"DESCRIBE deaths")
+    column_names = [row[0] for row in rel.fetchall()] 
+    con.close()
+    return jsonable_encoder({"columns": column_names})
+
+def unique_columns(col1: str, col2:str):
+    rel = con.sql(f"SELECT DISTINCT {col1}, {col2} FROM deaths;").fetchall()
+    con.close()
+    return rel
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from import_csv import create, show
+from import_csv import create, show, columns, unique_columns
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -13,5 +12,15 @@ async def lists():
     return {create()}
 
 @app.get("/show")
-async def lists():
+async def lists2():
     return show()
+
+@app.get("/columns")
+async def get_columns():
+    result = columns()
+    return result
+
+@app.get("/unique_columns")
+async def get_unique_columns(col1: str, col2: str):
+    result = unique_columns(col1, col2)
+    return result
